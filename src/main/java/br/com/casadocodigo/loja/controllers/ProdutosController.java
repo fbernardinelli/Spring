4bbf7f7e969aefa653f6075e61f957d7b57ab60e
fabiobernardinelli <fabiobernardinelli@192.168.2.105>
2016-10-25 22:07:59 -0200

    2

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,23 +1,28 @@
 package br.com.casadocodigo.loja.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.casadocodigo.loja.DAO.ProdutoDAO;
 import br.com.casadocodigo.loja.model.Produto;
 
 @Controller
 public class ProdutosController {
-	
-	@RequestMapping("/produtos/form")
-	public String form(){
-		return("/produtos/form");
-	}
-	
-	@RequestMapping("/produtos")
-	public String grava(Produto produto){
-		System.out.println(produto);
-		return("ok");
-	}
-	
 
+    @Autowired
+    private ProdutoDAO dao;
+
+    @RequestMapping("/produtos/form")
+    public String form() {
+        return "produtos/form";
+    }
+
+    @RequestMapping("/produtos")
+    public String grava(Produto produto) {
+
+        dao.gravar(produto);
+
+        return "produtos/ok";
+    }
 }
